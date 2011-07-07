85a6e9f4ec35580c40d494e402f60f46e053e76c
bokovskii <bokovskii@gmail.com>
2011-07-07 21:19:20 +0000

    fixed some warnings in the C++ project; fixed project properties for Release

--- CudaExports/CudaAlgebra.cu
+++ CudaExports/CudaAlgebra.cu
@@ -1,30 +1,32 @@
 
 #include "CudaAlgebra.cuh"
 
-
-void* CreateCublasHandle(void* streamId) throw(int)
+extern "C"
 {
-	cudaStream_t* strId = (cudaStream_t*) streamId;
-	cublasHandle_t* chandle = (cublasHandle_t*) malloc(sizeof(cublasHandle_t));
+	void* CreateCublasHandle(void* streamId) throw(int)
+	{
+		cudaStream_t* strId = (cudaStream_t*) streamId;
+		cublasHandle_t* chandle = (cublasHandle_t*) malloc(sizeof(cublasHandle_t));
 
-	CUBLAS_SAFE_CALL(cublasCreate(chandle));
-	CUBLAS_SAFE_CALL(cublasSetStream(*chandle, *strId));
+		CUBLAS_SAFE_CALL(cublasCreate(chandle));
+		CUBLAS_SAFE_CALL(cublasSetStream(*chandle, *strId));
 
-	return (void*) chandle;
-}
+		return (void*) chandle;
+	}
 
-void DestroyCublasHandle(void* cublasHandle) throw(int)
-{
-	cublasHandle_t* chandle = (cublasHandle_t*) cublasHandle;
+	void DestroyCublasHandle(void* cublasHandle) throw(int)
+	{
+		cublasHandle_t* chandle = (cublasHandle_t*) cublasHandle;
 	
-	CUBLAS_SAFE_CALL(cublasDestroy(*chandle));
+		CUBLAS_SAFE_CALL(cublasDestroy(*chandle));
 	
-	free(cublasHandle);
-}
+		free(cublasHandle);
+	}
 
-void CublasScalarMult(void* cublasHandle, float* devicePointer, int elemSpacing, int length, float* scalar) throw(int)
-{
-	cublasHandle_t* chandle = (cublasHandle_t*) cublasHandle;
+	void CublasScalarMult(void* cublasHandle, float* devicePointer, int elemSpacing, int length, float* scalar) throw(int)
+	{
+		cublasHandle_t* chandle = (cublasHandle_t*) cublasHandle;
 
-	CUBLAS_SAFE_CALL(cublasSscal(*chandle, length, scalar, devicePointer, elemSpacing));
+		CUBLAS_SAFE_CALL(cublasSscal(*chandle, length, scalar, devicePointer, elemSpacing));
+	}
 }
